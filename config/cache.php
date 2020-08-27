23225c9457297839c6696e83f7d939f8eafa9925
tianheng2017 <tianheng2017@gmail.com>
2020-08-27 11:40:53 +0800

    Update cache.php

--- config/cache.php
+++ config/cache.php
@@ -7,7 +7,7 @@ use think\facade\Env;
 
 return [
     // 默认缓存驱动
-    'default' => 'redis',
+    'default' => 'file',
 
     // 缓存连接方式配置
     'stores'  => [
@@ -17,17 +17,13 @@ return [
             // 缓存保存目录
             'path'       => '',
             // 缓存前缀
-            'prefix'     => 'food_',
+            'prefix'     => 'admin_',
             // 缓存有效期 0表示永久缓存
             'expire'     => 0,
             // 缓存标签前缀
             'tag_prefix' => 'tag:',
             // 序列化机制 例如 ['serialize', 'unserialize']
             'serialize'  => ['serialize', 'unserialize'],
-            // 端口
-            'port'       => 15537,
-            // 密码
-            'password'   => 'jn3sU7kq0s6zMc1L0PlaQjIx',
         ],
         'file' => [
             // 驱动方式
@@ -35,7 +31,7 @@ return [
             // 缓存保存目录
             'path'       => '',
             // 缓存前缀
-            'prefix'     => 'food_',
+            'prefix'     => 'admin_',
             // 缓存有效期 0表示永久缓存
             'expire'     => 0,
             // 缓存标签前缀
