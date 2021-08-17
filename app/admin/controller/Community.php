482844e83eff91fbd53b7461f085251bca43e665
Administrator <tianheng2017@qq.com>
2021-08-17 23:36:42 +0800

    change

--- app/admin/controller/Community.php
+++ app/admin/controller/Community.php
@@ -23,5 +23,28 @@ class Community extends AdminController
         
     }
 
+    /**
+     * @NodeAnotation(title="删除")
+     */
+    public function delete()
+    {
+        $ids = $this->request->param('id');
+        $row = $this->model->whereIn('id', $ids)->select();
+        $row->isEmpty() && $this->error('数据不存在');
+
+        //判断社区有无被引用
+        $count = \app\admin\model\Form1::whereIn('community', $ids)->count();
+        if ($count) {
+            $this->error('删除失败：该社区已被他处引用，请解除后再删');
+        }
+
+        try {
+            $save = $row->delete();
+        } catch (\Exception $e) {
+            $this->error('删除失败');
+        }
+        $save ? $this->success('删除成功') : $this->error('删除失败');
+    }
+
     
 }
