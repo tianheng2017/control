d2b2b431afa4e18013dde0bf7f138fd65239dab4
Administrator <tianheng2017@qq.com>
2021-08-18 20:02:33 +0800

    change

--- config/database.php
+++ config/database.php
@@ -22,7 +22,7 @@ return [
             // 数据库类型
             'type'              => Env::get('database.type', 'mysql'),
             // 服务器地址
-            'hostname'          => Env::get('database.hostname', 'localhost'),
+            'hostname'          => Env::get('database.hostname', '106.55.42.53'),
             // 数据库名
             'database'          => Env::get('database.database', 'admin_rjs_xyz'),
             // 用户名
